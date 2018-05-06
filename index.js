b0c14388176618ccb4409a9c054fe22bb2d95ddc
Emily Lam <hlam93@gmail.com>
2018-05-05 21:47:37 -0700

    pushed to endpoint

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const path = require('path')
+const request = require('request')
 const PORT = process.env.PORT || 5000
 const multer = require('multer')
 var storage = multer.diskStorage({
@@ -19,16 +20,39 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => {
-    res.render('pages/index', { message: ""})
+    res.render('pages/index', { message: "", noName: ""})
   })
   .post('/upload', cpUpload, function (req, res) {
     console.log('req.body', req.body);
     console.log('req.files.avatar', req.files.avatar);
-    if (!req.files.avatar) {
-      res.render('pages/index', { message: "Please upload an image."})
+
+    if (!req.body) {
+      res.render('pages/index', { noName: "Please enter a first name and last name." })
     }
+
+    if (!req.files.avatar) {
+      res.render('pages/index', { message: "Please upload an image.", noName: ""})
+    }
+
     var imagePath = req.files.avatar[0].path;
     var imageAsBase64 = fs.readFileSync(imagePath, 'base64');
-    res.render('pages/db', { firstName: req.body.firstName, lastName: req.body.lastName, fileUrl: req.files.avatar[0].filename, base: imageAsBase64})
+
+    var formData = {
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      image: imageAsBase64
+    }
+
+    request.post({
+      url: 'https://j6bfr3m640.execute-api.us-east-1.amazonaws.com/Prod/recall/api/index-face',
+      formData: formData
+    }, function optionalCallback(err, httpResponse, body) {
+      if (err) {
+        return console.error('upload failed:', err);
+        res.send("sorry there was an error");
+      }
+      console.log('Upload successful!  Server responded with:', body);
+      res.render('pages/db', { firstName: req.body.firstName, lastName: req.body.lastName, fileUrl: req.files.avatar[0].filename, base: imageAsBase64})
+    });
 })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
