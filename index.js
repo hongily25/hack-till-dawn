082db4901ac61461ce0c88402c833a8dec10f02d
Emily Lam <hlam93@gmail.com>
2018-05-06 08:54:54 -0700

    pushed to endpoint correctly

--- index.js
+++ index.js
@@ -129,4 +129,8 @@ express()
   .get('/video', (req, res) => {
     res.render('pages/video')
   })
+  .get('/download', function(req, res){
+    var file = __dirname + '/public/FullTarty.mp4';
+    res.download(file); // Set disposition and send it.
+  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
